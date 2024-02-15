21e539dd869adccf4bf94c4624dc603009cd913b
Efe-code1 <efezzy321@gmail.com>
2024-02-15 22:27:16 +0100

    85%done

---
+++ netflix.js
@@ -0,0 +1,11 @@
+// const nav = document.getElementsByClassName('showcase-top');
+// window.addEventListener('scroll', () => {
+//     if(window.scrollY >= 100) {
+//         nav.classList.add('nav-black');
+//     }else {
+//         nav.classList.remove('nav-black');
+//     }
+// })
+
+// alert ("NOTE: This is just a Netflix clone website, thereby any button clicked on this website directs you to the main Netflix website");
+
